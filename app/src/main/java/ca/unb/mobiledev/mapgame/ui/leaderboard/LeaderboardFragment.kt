1238d586cf40b95dbee137a0f13f9405cd8bf3ac
tom tait <thomas.t.ca@outlook.com>
2023-10-14 12:33:40 -0300

    Initial commit

---
+++ app/src/main/java/ca/unb/mobiledev/mapgame/ui/leaderboard/LeaderboardFragment.kt
@@ -0,0 +1,37 @@
+package ca.unb.mobiledev.mapgame.ui.leaderboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import ca.unb.mobiledev.mapgame.databinding.FragmentLeaderboardBinding
+
+class LeaderboardFragment : Fragment() {
+
+    private var _binding: FragmentLeaderboardBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val notificationsViewModel = ViewModelProvider(this)[LeaderboardViewModel::class.java]
+
+        _binding = FragmentLeaderboardBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
